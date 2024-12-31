ecccf05172eca6998f81444aaa501d82bd26e6fb
Justin Tian <justin.tian.372@gmail.com>
2024-12-30 21:59:31 -0500

    started working on email button/link

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function showAlert() {
     alert("justin.tian.372@gmail.com");
 }
+
+function copyLink() {
+    const tempInput = document.createElement("input");
+    document.body.appendChild(tempInput);
+    tempInput.value = "justin.tian.372@gmail.com"; // Replace with the actual link
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+
+    // Show the notification
+    const notification = document.getElementById("copy-notification");
+    notification.style.display = "block";
+
+    // Hide the notification after 2 seconds
+    setTimeout(function () {
+        notification.style.display = "none";
+    }, 2000);
+}
+
