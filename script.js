2a353ff531c781fdf3909553d8d57c6a247e6abb
Justin Tian <justin.tian.372@gmail.com>
2024-12-30 23:09:54 -0500

    added typing effect to hero

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ function copyLink() {
     }, 2000);
 }
 
+window.onload = function () {
+    const text = "Hello,";
+    const heroTextElement = document.getElementById('hero');
+    let index = 0;
+    let isDeleting = false;
+    let delay = 500;
+    let typingSpeed = 200;
+    let deletingSpeed = 200;
+
+    function typing() {
+        if (isDeleting) {
+            heroTextElement.textContent = text.substring(0, index);
+            index--;
+            if (index === 1) {
+                isDeleting = false;
+                setTimeout(typing, delay);
+            } else {
+                setTimeout(typing, deletingSpeed);
+            }
+        } else {
+            heroTextElement.textContent = text.substring(0, index);
+            index++;
+            if (index === text.length) {
+                isDeleting = true;
+                setTimeout(typing, delay);
+            } else {
+                setTimeout(typing, typingSpeed);
+            }
+        }
+    }
+    typing();
+}
